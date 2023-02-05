0e136c1a5254e95adde09371abda06e7665afdcf
raj19joshi <rajlaxmi.joshi@atos.net>
2023-02-05 21:33:44 +0100

    ngrx mock store

--- src/app/interactive-card/interactive-card.component.spec.ts
+++ src/app/interactive-card/interactive-card.component.spec.ts
@@ -23,3 +23,43 @@ describe('InteractiveCardComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { StoreModule, Store, select } from '@ngrx/store';
+// import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
+// import { MyComponent } from './my.component';
+// import * as fromRoot from './app.state';
+// import { initialState } from './app.state';
+
+// describe('MyComponent', () => {
+//   let component: MyComponent;
+//   let fixture: ComponentFixture<MyComponent>;
+//   let store: MockStore<fromRoot.State>;
+//   const selectMyState = (state: fromRoot.State) => state.myState;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [StoreModule.forRoot({})],
+//       declarations: [MyComponent],
+//       providers: [provideMockStore({ initialState })]
+//     });
+
+//     fixture = TestBed.createComponent(MyComponent);
+//     component = fixture.componentInstance;
+//     store = TestBed.inject(Store);
+//     spyOn(store, 'pipe').and.returnValue(of({}));
+//     spyOn(store, 'dispatch').and.callThrough();
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+
+//   it('should call the selector', () => {
+//     store.pipe(select(selectMyState)).subscribe();
+//     expect(store.pipe).toHaveBeenCalledWith(select(selectMyState));
+//   });
+// });
+
